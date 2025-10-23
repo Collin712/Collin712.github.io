29746618950aebcc7536be5ea1c26aa80aefc73c
Collin712 <collinschoolcodingclassemail@gmail.com>
2025-10-23 16:27:23 +0000

    circularity

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,18 +20,30 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle;
+        var circles = [];
 
 
 
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle);
+        }
         
 
 
         // TODO 3 : Call the drawCircle() function
+        for(var i=1;i<=5;i++){
+            drawCircle();
+        }
 
 
 
         // TODO 7 : Use a loop to create multiple circles
+
 
 
 
@@ -47,10 +59,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
+            for(var i=0;i<circles.length;i++){
+                physikz.updatePosition(circles[i]);
+            }
 
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
+            for(var i=0;i<circles.length;i++){
+                game.checkCirclePosition(circles[i]);
+            }
 
             // TODO 8 / TODO 9 : Iterate over the array
            
@@ -70,8 +87,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+            if ( circle.x < 0 ) {
+                circle.x = canvas.width;
+            }
+            if ( circle.y < 0 ) {
+                circle.y = canvas.height;
+            }
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
